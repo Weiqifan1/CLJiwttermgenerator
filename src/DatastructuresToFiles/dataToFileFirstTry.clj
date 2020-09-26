cfb0dee9da24c97a21c634620240cf66a1aa66fe
Weiqifan1 <cmlykke@hotmail.com>
2020-09-26 18:49:38 +0200

    ready to create strings to write to file

--- src/DatastructuresToFiles/dataToFileFirstTry.clj
+++ src/DatastructuresToFiles/dataToFileFirstTry.clj
@@ -1,9 +1,31 @@
 
+(load-file "src/StoryAnalysing/readInfoFromFile.clj")
+(load-file "src/ParsingStories/storyToDatastructure.clj")
+
 (println "dataToFIleFirstTry")
 
 ;; This program displays Hello World
 (defn writeExample []
-  (with-open [w (clojure.java.io/writer "resources/ankiResultFiles/Example.txt" :append true)]
-    (.write w (str "hello" "world"))))
+  (with-open [w (clojure.java.io/writer "resources/ankiResultFiles/Example.txt" :append false)]
+    (.write w (str "hello" "world" "there"))))
+
+(def raw_story (clean_story_funk "src\\lwttermgenerator\\Avengers01_01_changedToTrad.txt"))
+(def miniStory (take 11 raw_story))
+;(println miniStory)
+;(println (take 6 raw_story))
+;;(println (contentVecGnerator miniStory))
+;;;(println (contentVecGnerator raw_story))
+(def dataStrudtuceToWriteToFile (contentVecGnerator miniStory))
+
+;(println (count dataStrudtuceToWriteToFile))
+;(println dataStrudtuceToWriteToFile) ;virker
+
+(defn createStringToBeWrittenTofileFromContentVecGenerator
+  [inputDataStructure]
+  (let [hashmapWithLineInfo (nth inputDataStructure 3)]
+    (identity hashmapWithLineInfo)))
+
+
+(println (createStringToBeWrittenTofileFromContentVecGenerator dataStrudtuceToWriteToFile))
 
 ;slut
