2917bc35be63ed0545bdbbc2d5d63147e3411093
Weiqifan1 <cmlykke@hotmail.com>
2020-10-04 22:12:18 +0200

    must rewrite the code to generate nested String arrays

--- src/DatastructuresToFiles/dataToFileSecondTry.clj
+++ src/DatastructuresToFiles/dataToFileSecondTry.clj
@@ -22,12 +22,35 @@
     sortedInfoLines)
   ))
 
+(defn contentVec-bata-3 [inputStory]
+  (filter #(not (nil? %))
+  (map-indexed
+      (fn [lineIndex, eachLine]
+        (let [previousLine (nth inputStory (helper_rundOpTilNul (- lineIndex 1)))
+              storyLineIsChinese (and (helper_stringHasChinese eachLine) (helper_stringHasOnlyNumPunktAndNotEmpty previousLine))
+              ]
+          (if storyLineIsChinese
+         (hash-map :line eachLine
+                   :lineID previousLine
+                   :storyFirstLine (nth inputStory 0)
+                   :storySecondLine (nth inputStory 1)
+                   :storyThirdLine  (nth inputStory 2)
+                   :components (linetoUniqueWordsAndChars eachLine);(map createCharInfoHashMap  (linetoUniqueWordsAndChars (get % :line))); ;outCommented Vector
+                   :componentsToBeRemovedIfDublet (linetoUniqueWordsAndChars eachLine)
+                   :lineInfo (tradKinStringToTradWordsInfoStringFull eachLine)
+                   :linePinYin (tradKinStringToPinyinKinStringWith* eachLine)
+                   :lineCharsAndTzaiNumbers (stringToCharsAndTzaiNumber eachLine)
+                   :lineSortedTzaiNumbers (vec (reverse (stringToTzaiNumbersSortedNoNil eachLine)))
+                  ) nil))
+        )
+      inputStory)))
+
+
 (defn newstoryfilePathToAnkiFile [storyFilePath targetAnkiFilepath] ;"src\\lwttermgenerator\\Avengers01_01_changedToTrad.txt"
   (let [
         fullStory (clean_story_funk storyFilePath)
-        simpelLineHashMap (contentVec-bata-1 fullStory)
-        removeNonChineseLineMaps (contentVec-bata-2 simpelLineHashMap)
-        sortedInfoLines (sortByTzaiNumbers removeNonChineseLineMaps)
+        simpelLineHashMap (contentVec-bata-3 fullStory)
+        sortedInfoLines (sortByTzaiNumbers simpelLineHashMap)
         ;removeDublicates (removeIdenticalLinesThatOccurLater sortedInfoLines)
         nestedWordAndCharList (map #(get % :componentsToBeRemovedIfDublet) sortedInfoLines)
         AccumWordAndChar (vec (map
@@ -51,14 +74,21 @@
         addUniqueWordsAndCarsToMaps (map-indexed
                                       (fn [eachIndex eachMap]
                                         (conj eachMap {:newWordsAndChars (nth newUniqueCharactersLists eachIndex)}))
-                                      sortedInfoLines)
+                                     sortedInfoLines)
         ;2020-10-04 nedenstaaende funktion virker
         removeMapsWithNoNewWordsOrChars (filter
-                                          #(< 0 (count (% :newWordsAndChars)))
+                                          #(not (= [] (% :newWordsAndChars)))
                                           addUniqueWordsAndCarsToMaps)
         ]
-    (println (count sortedInfoLines))
+    ;(println (nth addUniqueWordsAndCarsToMaps 50))
+    (println (count addUniqueWordsAndCarsToMaps))
+    (println (take 10 (map #(% :newWordsAndChars) addUniqueWordsAndCarsToMaps)))
+
     (println (count removeMapsWithNoNewWordsOrChars))
+    (println (take 10 (map #(% :newWordsAndChars) removeMapsWithNoNewWordsOrChars)))
+    
+    (println (count (flatten (map #(% :newWordsAndChars) removeMapsWithNoNewWordsOrChars))))
+    (println (take 10 (flatten (map #(% :newWordsAndChars) removeMapsWithNoNewWordsOrChars))))
 
     ;我
     ;這是我的
